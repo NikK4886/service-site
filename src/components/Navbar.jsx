f4c77dfdca3cc6d9f2d168bc126ffb3bb1fc0da3
Nikolas k <nkaer100@uottawa.ca>
2026-06-07 17:37:49 -0400

    bug fixes 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <header className="navbar">
-            <Link to="/" className="navbar-logo">
+            <Link to="/service-site/" className="navbar-logo">
                 <img src="/images/logo.png" alt="King Maker logo" />
                 <span> King Maker </span>
             </Link>
 
             <nav className="navbar-links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
-                <Link to="/offers">Offers</Link>
-                <Link to="/book_bg" className="nav-book-btn">
+                <Link to="/service-site/">Home</Link>
+                <Link to="/service-site/about">About Us</Link>
+                <Link to="/service-site/offers">Offers</Link>
+                <Link to="/service-site/book_bg" className="nav-book-btn">
                     Book a Session!
                 </Link>
             </nav>
